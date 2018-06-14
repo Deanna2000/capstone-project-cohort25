cba7825277425c642b8c5cf330ae0c0f5f17bee6
Deanna Vickers <deanna2000@gmail.com>
2018-06-13 19:35:55 -0500

    added items display

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import DisplayFriends from './Friends/DisplayFriends'
+// import DisplayFriends from './Friends/DisplayFriends'
+import DisplayItems from './Items/DisplayItems'
 
 class Dashboard extends Component {
   render() {
@@ -9,7 +10,8 @@ class Dashboard extends Component {
         <header className="Dash-header">
           <h1 className="Dash-title"> Dashboard for Logged In View</h1>
           <p className="welcome">Welcome {userName.fName}</p>
-          <DisplayFriends ActiveUser={userName}/>
+          {/* <DisplayFriends ActiveUser={userName}/> */}
+          <DisplayItems ActiveUser={userName}/>
         </header>
       </div>
     );
--- src/containers/Dashboard/Friends/DisplayFriends.js
+++ src/containers/Dashboard/Friends/DisplayFriends.js
@@ -10,71 +10,69 @@ class DisplayFriends extends Component {
 
   //
   unique = 1
-  // Get the userid of the currently logged in user
-  // 1. Search friend requester for current user
-  // Get the requested person
-  // 2. Search requested person for current user
-  // Get the friend requester
-  // 3. Show that list of users from each friendship object
-  // const userItem = sessionStorage.getItem('id')
-  // console.log({userItem})
+
   componentDidMount() {
-    //embed the requestedpersonID in this fetch call
     const friendsListTemp = []
-    const friends = []
-    const friends2 = []
     const allFriends = []
+    // Fetch all instances of the Accepter ID matching the logged in user
     fetch(`http://localhost:5001/friendsRelationships?AccepterId=${this.props.ActiveUser.id}`)
       .then(response => response.json())
       .then(friendShips => {
-        console.log("friendShips", friendShips)
+        console.log("friendShips", friendShips) //should be 2 objects
+        // Loop over the 2 objects and get the ID of the requester
         friendShips.forEach(element => {
           const connection = element.RequesterId
-          console.log("connectionreq",connection)
+          console.log("connectionreq",connection) // should be 2 ID's
+          // Fetch the users whose ID's match the connections above (2)
+
           return fetch(`http://localhost:5001/users?id=${connection}`)
           .then(response => response.json())
           .then(connectedFriends => {
+            // add those users to an array called allfriends
             allFriends.push(connectedFriends)
-            console.log("connected friends for requesters", connectedFriends)
-            console.log("all friends first time", allFriends)
+            console.log("connected friends for requesters", connectedFriends) //should return 2 user objects
+            console.log("all friends first time", allFriends) // should contain 2 user objects
 
 
 
-        fetch(`http://localhost:5001/friendsRelationships?RequesterId=${this.props.ActiveUser.id}`)
-          .then(response => response.json())
-          .then(friendShips2 => {
-            friendShips2.forEach(f => {
-              console.log("f",f)
-              friends2.push(f)
+            // Fetch the instances of requesters that match the current user
+            fetch(`http://localhost:5001/friendsRelationships?RequesterId=${this.props.ActiveUser.id}`)
+            .then(response => response.json())
+            .then(friendShips2 => {
+              // Loop over the 1 object to get the ID's of the accepters
+              friendShips2.forEach(element => {
+                const connectionAcc = element.AccepterId
+                console.log("connectionacc", connectionAcc) // shows ID's of accepters (1)
 
-          })
-            friends2.forEach(element => {
-              const connection = element.AccepterId
-              console.log("connectionacc", connection)
-
-              return fetch(`http://localhost:5001/users?id=${connection}`)
-              .then(response => response.json())
-              .then(connectedFriends2 => {
-                allFriends.push(connectedFriends2)
-                console.log("allfriends second time",allFriends)
-                  console.log("connected friends for accepters", connectedFriends)
+                // Fetch the user objects for the users whose ID's match the connectionAcc
+                return fetch(`http://localhost:5001/users?id=${connectionAcc}`)
+                .then(response => response.json())
+                .then(connectedFriends2 => {
+                  // Add those users to an array called allFriends
+                  allFriends.push(connectedFriends2)
+                  console.log("allfriends second time",allFriends)
+                  console.log("connected friends for accepters", connectedFriends2)
                   // ConnectedFriends contains the list of friends that have
-                  // requestsed the logged in user
-                  allFriends.forEach(element => {
-                    //push friends into something temporary and pull all to friendList at the end
-                    friendsListTemp.push(element.fName + " " + element.lName)
-                  })
-                  this.setState({ friendsList: friendsListTemp })
+                  // requested the logged in user
                 })
-              })
-              })
-            }
-            )
+              })// closes connectedFriends2
+            })// closes FriendShips2 foreach
+            allFriends.forEach(element => {
+              console.log("allfriends elements", element)
+              const fullName = element.fName + " " + element.lName
+              console.log("fullname", fullName)
+              //push friends into something temporary and pull all to friendList at the end
+              friendsListTemp.push(fullName)
+        console.log("flt", friendsListTemp)
+        this.setState({ friendsList: friendsListTemp })
+          })// closes FriendShips2 json object
+        })// closes ConnectedFriends
+        })//closes FriendShips foreach
+      })//closes Friendships json object
 
-          })
-      })
     console.log("state", this.state)
   }
+
 
   render() {
     return (
